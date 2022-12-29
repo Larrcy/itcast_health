654f3c9f3b2a229211f428b06ce1736d311d8862
root <841407722@qq.com>
2022-12-29 12:24:11 +0800

    2022.12.29

--- health_parent/health_common/src/test/java/com/itheima/test/POITest.java
+++ health_parent/health_common/src/test/java/com/itheima/test/POITest.java
@@ -9,10 +9,7 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 public class POITest {
     @Test
@@ -38,17 +35,49 @@ public class POITest {
 
         //获取工作表，既可以根据工作表的顺序获取，也可以根据工作表的名称获取
         XSSFSheet sheet = workbook.getSheetAt(0);
-//获取当前工作表最后一行的行号，行号从0开始
+        //获取当前工作表最后一行的行号，行号从0开始
         int lastRowNum = sheet.getLastRowNum();
         for (int i = 0; i <= lastRowNum; i++) {
             //根据行号获取行对象
             XSSFRow row = sheet.getRow(i);
+            //获取当前行最后一个单元格索引
             short lastCellNum = row.getLastCellNum();
             for (short j = 0; j < lastCellNum; j++) {
                 String value = row.getCell(j).getStringCellValue();
                 System.out.println(value);
             }
         }
+        workbook.close();
+    }
+
+    @Test
+    public void Test3() throws Exception {
+        XSSFWorkbook workbook = new XSSFWorkbook();
+        //创建工作表，指定工作表名称
+        XSSFSheet sheet = workbook.createSheet("传智播客");
+
+        //创建行，0表示第一行
+        XSSFRow row = sheet.createRow(0);
+        //创建单元格，0表示第一个单元格
+        row.createCell(0).setCellValue("编号");
+        row.createCell(1).setCellValue("名称");
+        row.createCell(2).setCellValue("年龄");
+
+        XSSFRow row1 = sheet.createRow(1);
+        row1.createCell(0).setCellValue("1");
+        row1.createCell(1).setCellValue("小明");
+        row1.createCell(2).setCellValue("10");
+
+        XSSFRow row2 = sheet.createRow(2);
+        row2.createCell(0).setCellValue("2");
+        row2.createCell(1).setCellValue("小王");
+        row2.createCell(2).setCellValue("20");
+
+        //通过输出流将workbook对象下载到磁盘
+        FileOutputStream out = new FileOutputStream("C:\\Users\\84140\\Desktop\\itcast.xlsx");
+        workbook.write(out);
+        out.flush();
+        out.close();
         workbook.close();
     }
 }
